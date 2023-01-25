ecff7cd2ffd53a54673ef24826418c79c166c9e3
Arif Hossan <arif.hossan.eu@gmail.com>
2023-01-26 00:27:20 +0600

    delete,put,update

--- js/fetch.js
+++ js/fetch.js
@@ -2,7 +2,7 @@
 //       .then(response => response.json())
 //       .then(json => console.log(json))
 function loadData() {
-  const url ="https://jsonplaceholder.typicode.com/todos/1";
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
   fetch(url)
     .then((response) => response.json())
     .then((json) => console.log(json));
@@ -13,3 +13,42 @@ function loadData() {
     .then((response) => response.json())
     .then((json) => console.log(json));
 }*/
+
+function deleteAPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  });
+}
+
+function createAPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function updateAPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PUT",
+    body: JSON.stringify({
+      id: 1,
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
